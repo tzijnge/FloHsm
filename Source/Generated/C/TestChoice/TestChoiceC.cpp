29e8407c6173e594b3f619354ca9dd8cb4b0cf13
tzijnge <someone@somewhere.com>
2020-11-04 09:41:48 +0100

    Support for C state machines * Extended TestChoice

--- Source/Generated/C/TestChoice/TestChoiceC.cpp
+++ Source/Generated/C/TestChoice/TestChoiceC.cpp
@@ -13,8 +13,11 @@ namespace C
       ChoiceInitAction(&choice, ChoiceAction_a1, &A1);
       ChoiceInitAction(&choice, ChoiceAction_a2, &A2);
       ChoiceInitAction(&choice, ChoiceAction_a3, &A3);
+      ChoiceInitAction(&choice, ChoiceAction_a4, &A4);
+      ChoiceInitAction(&choice, ChoiceAction_a5, &A5);
 
       ChoiceInitGuard(&choice, ChoiceGuard_g1, &G1);
+      ChoiceInitGuard(&choice, ChoiceGuard_g2, &G2);
 
       ChoiceInitStateMachine(&choice);
     }
@@ -27,6 +30,7 @@ namespace C
     }
 
     bool g1{false};
+    bool g2{false};
 
     ChoiceInstance choice;
 
@@ -35,7 +39,10 @@ namespace C
     static void A1(void* context) { static_cast<TestChoice*>(context)->AddToLog("A1"); }
     static void A2(void* context) { static_cast<TestChoice*>(context)->AddToLog("A2"); }
     static void A3(void* context) { static_cast<TestChoice*>(context)->AddToLog("A3"); }
+    static void A4(void* context) { static_cast<TestChoice*>(context)->AddToLog("A4"); }
+    static void A5(void* context) { static_cast<TestChoice*>(context)->AddToLog("A5"); }
     static bool G1(void* context) { return static_cast<TestChoice*>(context)->g1; }
+    static bool G2(void* context) { return static_cast<TestChoice*>(context)->g2; }
 
     void AddToLog(const std::string& l)
     {
@@ -49,12 +56,22 @@ namespace C
   TEST_F(TestChoice, choice1)
   {
     g1 = false;
+    g2 = false;
     Choice_e1(&choice);
-    EXPECT_EQ("A1:A3", Log());
+    EXPECT_EQ("A1:A3:A5", Log());
     EXPECT_EQ("S3", CurrentStateName());
   }
 
   TEST_F(TestChoice, choice2)
+  {
+    g1 = false;
+    g2 = true;
+    Choice_e1(&choice);
+    EXPECT_EQ("A1:A3:A4", Log());
+    EXPECT_EQ("S2", CurrentStateName());
+  }
+
+  TEST_F(TestChoice, choice3)
   {
     g1 = true;
     Choice_e1(&choice);
